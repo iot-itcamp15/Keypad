0131f052f9a1872209fd6f4636feecd72d9a4aaa
Napat Arayawattanapong <41178313+NAPATKRUP@users.noreply.github.com>
2019-05-28 13:57:59 +0700

    Upload keypad file and library file

---
+++ keypad.ino
@@ -0,0 +1,31 @@
+/* Keypad
+การต่อขาใน keypad
+ขาในบอร์ด    D0 D1 D2 D3 D4 D5 D6
+ขาใน Keypad 7  6  5  4  3  2  1
+*/
+#include <Keypad.h>
+
+const byte ROWS = 4; //4 แถว
+const byte COLS = 3; //3 หลัก
+char keys[ROWS][COLS] = {
+  {'1','2','3'},
+  {'4','5','6'},
+  {'7','8','9'},
+  {'*','0','#'}
+};
+byte rowPins[ROWS] = {D0, D1, D2, D3}; //เชื่อมต่อแถว ขาที่จะส่งค่าไปยัง keypad
+byte colPins[COLS] = {D4, D5, D6}; //เชื่อมต่อหลัก ขาที่จะส่งค่าไปยัง keypad
+
+Keypad keypad = Keypad( makeKeymap(keys), rowPins, colPins, ROWS, COLS );
+
+void setup(){
+  Serial.begin(9600);
+}
+  
+void loop(){
+  char key = keypad.getKey();
+  
+  if (key){
+    Serial.println(key);
+  }
+}
